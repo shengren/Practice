a293fc921e7e876b426348277d3b406c53c0831e
Shengren Li <shengrenli12@gmail.com>
2012-09-30 15:59:47 -0700

    add 1

---
+++ leetcode/Depth_of_Binary_Tree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int max_height;
+    void dfs(TreeNode t, int h) {
+        if (t == null) return;
+        if (h > max_height) max_height = h;
+        dfs(t.left, h + 1);
+        dfs(t.right, h + 1);
+    }
+    public int maxHeight(TreeNode root) {
+        max_height = 0;
+        dfs(root, 1);
+        return max_height;
+    }
+}
