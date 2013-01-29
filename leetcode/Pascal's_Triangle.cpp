0d1006c9d947189c4de1ca98fbd8e8f12c9820f2
Shengren Li <shengrenli12@gmail.com>
2013-01-29 14:16:27 -0800

    Updating 2.

--- leetcode/Pascal's_Triangle.cpp
+++ leetcode/Pascal's_Triangle.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     vector<vector<int> > generate(int numRows) {
-        vector<vector<int> > ret;
+        vector<vector<int> > tri;
         for (int i = 1; i <= numRows; ++i) {
-            if (i == 1) {
-                vector<int> r1;
-                r1.push_back(1);
-                ret.push_back(r1);
-            } else if (i == 2) {
-                vector<int> r2;
-                r2.push_back(1);
-                r2.push_back(1);
-                ret.push_back(r2);
-            } else {
-                vector<int> cr;
-                for (int j = 0; j < i; ++j) {
-                    if (j == 0 || j == i - 1) {
-                        cr.push_back(1);
-                    } else {
-                        cr.push_back(ret[i - 2][j - 1] + ret[i - 2][j]);
-                    }
-                }
-                ret.push_back(cr);
-            }
-        }        
-        return ret;
+            vector<int> new_row;
+            new_row.push_back(1);
+            for (int j = 2; j < i; ++j)
+                new_row.push_back(tri[i - 2][j - 2] + tri[i - 2][j - 1]);
+            if (i > 1)
+                new_row.push_back(1);
+            tri.push_back(new_row);
+        }
+        return tri;
     }
 };
--- leetcode/Pascal's_Triangle_II.cpp
+++ leetcode/Pascal's_Triangle_II.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int> ret;
-        ret.push_back(1);
+    vector<int> getRow(int rowIndex) {  // rowIndex is 0-based
+        vector<int> row;
+        row.push_back(1);
         for (int r = 1; r <= rowIndex; ++r) {
             int pre = 1;
             for (int i = 1; i < r; ++i) {
-                int cur = pre + ret[i];
-                pre = ret[i];
-                ret[i] = cur;
+                int cur = pre + row[i];
+                pre = row[i];
+                row[i] = cur;
             }
-            ret.push_back(1);
+            row.push_back(1);
         }
-        return ret;
+        return row;
     }
 };
