975d682e25719a6a00ee800ce23d68975318b2ce
Shengren Li <shengrenli12@gmail.com>
2013-10-10 19:15:54 -0700

    Updating 1

--- leetcode/Single_Number.cpp
+++ leetcode/Single_Number.cpp
@@ -1,15 +1,14 @@
 class Solution {
   public:
     int singleNumber(int A[], int n) {
-      vector<bool> bit(n, false);
-      for (int i = 0; i < n; ++i)
-        for (int j = 0; j < 32; ++j)
-          if (A[i] & (1 << j))
-            bit[j] = !bit[j];
-      int singular = 0;
-      for (int j = 0; j < 32; ++j)
-        if (bit[j])
-          singular |= (1 << j);
-      return singular;
+      class Solution {
+        public:
+          int singleNumber(int A[], int n) {
+            int singular = 0;
+            for (int i = 0; i < n; ++i)
+              singular ^= A[i];
+            return singular;
+          }
+      };
     }
 };
