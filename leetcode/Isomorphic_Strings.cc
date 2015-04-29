36d75e672c17217872528ee73aab3360940e5b38
Shengren Li <shengrenli12@gmail.com>
2015-04-29 15:11:57 -0700

    Add 1 solution.

---
+++ leetcode/Isomorphic_Strings.cc
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  bool isIsomorphic(string s, string t) {
+    unordered_map<char, char> s2t, t2s;
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (s2t.find(s[i]) == s2t.end() && t2s.find(t[i]) == t2s.end()) {
+        s2t[s[i]] = t[i];
+        t2s[t[i]] = s[i];
+      } else if (s2t.find(s[i]) != s2t.end() && t2s.find(t[i]) != t2s.end()) {
+        if (s2t[s[i]] != t[i] || t2s[t[i]] != s[i])
+          return false;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
